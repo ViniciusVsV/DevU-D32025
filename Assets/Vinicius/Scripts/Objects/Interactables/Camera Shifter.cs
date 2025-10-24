613e3d80053519036b8b65e1e777b05930ecc015
ViniciusVsV <viniciusvsv193@gmail.com>
2025-10-24 16:04:13 -0300

    Melhorando Camera Shifter

--- Assets/Vinicius/Scripts/Objects/Interactables/Camera Shifter.cs
+++ Assets/Vinicius/Scripts/Objects/Interactables/Camera Shifter.cs
@@ -6,25 +6,18 @@ namespace Objects.Interactables
     public class CameraShifter : MonoBehaviour
     {
         [Header("||==== Objects ====||")]
-        [SerializeField] private CinemachineCamera cam; //Serializable para caso queira ver o gizmos da câmera no editor
-        [SerializeField] private Transform focusPoint;
+        [SerializeField] private CinemachineCamera newCamera;
 
-        private CinemachineFollow cinemachineFollow;
+        private CinemachineCamera baseCamera;
         private Rigidbody2D playerRb;
 
         [Header("||==== Parameters ====||")]
-        [SerializeField] private float newCameraSize;
         [SerializeField] private float timeToActivate;
-        private float baseCameraSize;
         private float timer;
-
-        public bool showGizmos; //Habilta mostrar o gizmos
 
         private void Start()
         {
-            cam = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineCamera>();
-            cinemachineFollow = cam.gameObject.GetComponent<CinemachineFollow>();
-            baseCameraSize = cam.Lens.OrthographicSize;
+            baseCamera = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineCamera>();
         }
 
         private void Update()
@@ -37,14 +30,15 @@ namespace Objects.Interactables
                 else
                 {
                     timer = 0;
-                    cam.Lens.OrthographicSize = baseCameraSize;
-                    cam.Follow = playerRb.transform;
+
+                    baseCamera.Priority = 10;
+                    newCamera.Priority = 0;
                 }
 
                 if (timer >= timeToActivate)
                 {
-                    cam.Lens.OrthographicSize = newCameraSize;
-                    cam.Follow = focusPoint;
+                    baseCamera.Priority = 0;
+                    newCamera.Priority = 10;
                 }
             }
         }
@@ -62,47 +56,10 @@ namespace Objects.Interactables
 
                 playerRb = null;
 
-                cam.Follow = other.transform;
+                baseCamera.Priority = 10;
+                newCamera.Priority = 0;
             }
         }
 
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            if (cam != null)
-                cinemachineFollow = cam.GetComponent<CinemachineFollow>();
-        }
-#endif
-
-        private void OnDrawGizmos()
-        {
-            if (!showGizmos || cam == null)
-                return;
-
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, focusPoint.position);
-
-            var lens = cam.Lens;
-            if (lens.Orthographic)
-            {
-                float height = newCameraSize * 2f;
-                float width = height * lens.Aspect;
-
-                Vector3 camPos = focusPoint.position;
-
-                camPos += cinemachineFollow.FollowOffset;
-
-                Vector3 topLeft = camPos + new Vector3(-width / 2f, height / 2f, 0f);
-                Vector3 topRight = camPos + new Vector3(width / 2f, height / 2f, 0f);
-                Vector3 bottomLeft = camPos + new Vector3(-width / 2f, -height / 2f, 0f);
-                Vector3 bottomRight = camPos + new Vector3(width / 2f, -height / 2f, 0f);
-
-                Gizmos.color = Color.yellow;
-                Gizmos.DrawLine(topLeft, topRight);
-                Gizmos.DrawLine(topRight, bottomRight);
-                Gizmos.DrawLine(bottomRight, bottomLeft);
-                Gizmos.DrawLine(bottomLeft, topLeft);
-            }
-        }
     }
 }
