0ccc04b913e89f3e3ab3874b57e2c0084e2e990e
allangmrs <allan81.guima@gmail.com>
2025-10-25 01:51:28 -0300

    dialogo agora ta poggers

--- Assets/Allan/Scripts/BossStateMachine/Boss/Drums/DrumSolo.cs
+++ Assets/Allan/Scripts/BossStateMachine/Boss/Drums/DrumSolo.cs
@@ -12,19 +12,24 @@ public class DrumSolo : MonoBehaviour , IRythmSyncable
 
     [Header("-----Ground----")]
     [SerializeField] public Transform groundTransform;
+    [SerializeField] public Rigidbody2D groundRigidbody;
     [SerializeField] public Transform originalPosition;
     
     [SerializeField] public Ease easeUp;
     [SerializeField] public Ease easeDown;
     [SerializeField] public Transform bouncePosition;
-    [SerializeField] public float upDuration;
+    [SerializeField] public float upDuration = 0.15f;
 
     [Header("-----Ground HitBox-----")]
     [SerializeField] public BoxCollider2D boxCollider;
     [SerializeField] public float hitboxActiveTime;
     [Range(0, 1)][SerializeField] private float beatPercentage;
     public List<ParticleSystem> ceilingFX;
-    private CinemachineImpulseSource impulseSource;
+    [SerializeField] private CinemachineImpulseSource impulseSource;
+    [SerializeField] private CinemachineImpulseSource rumblingSource;
+    [SerializeField] private float rumblingDuration;
+    [SerializeField] public float downDuration = 0.5f; // descida suave
+    private bool isMoving;
 
     private int beatCounter;
     private float beatLength;
@@ -58,29 +63,83 @@ public class DrumSolo : MonoBehaviour , IRythmSyncable
         if (beatCounter == 0 && isSoloing == true)
         {
             StartCoroutine(EndDrumSolo());
+            rumblingSource.GenerateImpulse();
+
+            foreach (ParticleSystem particles in ceilingFX)
+                particles.Play();
+
+            StartCoroutine(MoveGround());
+            /*StartCoroutine(EndDrumSolo());
             groundTransform.DOKill();
-            impulseSource.GenerateImpulse();
-            foreach(ParticleSystem particles in ceilingFX)
+
+            rumblingSource.GenerateImpulse();
+
+
+
+            foreach (ParticleSystem particles in ceilingFX)
             {
                 particles.Play();
             }
 
-            var sequence = DOTween.Sequence();
+            /*var sequence = DOTween.Sequence();
             sequence.Append(groundTransform.DOMove(bouncePosition.position, beatLength * beatPercentage).SetEase(easeUp)
-                .OnComplete(() => { boxCollider.enabled = false; })
+                .OnComplete(() => { StartCoroutine(ActivateHitbox()); })
             );
-            sequence.Append(groundTransform.DOMove(originalPosition.position, beatLength * 3).SetEase(easeDown));
             
-        }
-    }
+            sequence.Append(groundTransform.DOMove(originalPosition.position, beatLength * 3).SetEase(easeDown));
+            */
+            //sequence.SetUpdate(UpdateType.Fixed);
 
+            /*groundTransform.position = Vector3.Lerp(originalPosition.position, bouncePosition.position, beatLength);
+            groundTransform.position = Vector3.Lerp(bouncePosition.position, originalPosition.position, beatLength / 4);*/
+        }
+        if (beatCounter == 3)
+        {
+            StartCoroutine(MoveGround());
+        }
+        
+    }
+    private IEnumerator MoveGround()
+    {
+        isMoving = true;
+
+        // Subida brusca
+        Vector3 startPos = groundTransform.position;
+        Vector3 targetUp = bouncePosition.position;
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / upDuration;
+            groundTransform.position = Vector3.Lerp(startPos, targetUp, t);
+            yield return null;
+        }
+
+        // Ativa hitbox durante o impacto
+        StartCoroutine(ActivateHitbox());
+
+        // Descida suave
+        t = 0f;
+        Vector3 targetDown = originalPosition.position;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / downDuration;
+            // movimento com suavização (ease-out)
+            float smoothT = Mathf.SmoothStep(0f, 1f, t);
+            groundTransform.position = Vector3.Lerp(targetUp, targetDown, smoothT);
+            yield return null;
+        }
+
+        groundTransform.position = targetDown;
+        isMoving = false;
+    }
     private IEnumerator ActivateHitbox()
     {
         boxCollider.enabled = true;
         yield return new WaitForSeconds(hitboxActiveTime);
         boxCollider.enabled = false;
     }
-
     private IEnumerator EndDrumSolo()
     {
         yield return new WaitForSeconds(soloDuration);
--- Assets/Allan/Scripts/NPCDialogue.cs
+++ Assets/Allan/Scripts/NPCDialogue.cs
@@ -1,102 +1,32 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System;
 
 public class NPCDialogue : MonoBehaviour
 {
-    public GameObject dialoguePanel;
-    public GameObject continueButton;
-    public float wordSpeed;
-    public Text dialogueText;
-    public string[] dialogue;
-    public Text NPCNameText;
-    public string NPCName;
-    public GameObject interactMenu;
-    private int index;
-    public bool playerInRange;
-    private Coroutine isTyping;
+    public string npcName;
+    [TextArea(2, 5)] public string[] dialogueLines;
+    private bool playerInRange;
+
+    //public GameObject interactMenu;
 
     void Update()
     {
-        if (playerInRange)
-            interactMenu.SetActive(true);
-        else
-            interactMenu.SetActive(false);
-        if ((Input.GetButtonDown("Interact") || Input.GetButtonDown("Interact1")) && playerInRange)
+        //interactMenu.SetActive(playerInRange);
+
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            if (dialoguePanel.activeInHierarchy)
-            {
-                zeroText();
-            }
-            else
-            {
-                dialoguePanel.SetActive(true);
-                NPCNameText.text = NPCName;
-                isTyping = StartCoroutine(Typing());
-            }
-        }
-        
-        if(dialogueText.text == dialogue[index])
-        {
-            continueButton.SetActive(true);
+            DialogueManager.Instance.StartDialogue(dialogueLines, npcName);
         }
     }
 
-    public void zeroText()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (isTyping != null)
-        {
-            StopCoroutine(isTyping);
-        }
-        dialogueText.text = "";
-        index = 0;
-        dialoguePanel.SetActive(false);
-    }
-
-    IEnumerator Typing()
-    {
-        foreach (char letter in dialogue[index].ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(wordSpeed);
-        }
-    }
-
-    public void NextLine()
-    {
-        if (isTyping != null)
-        {
-            StopCoroutine(isTyping);
-        }
-        
-        continueButton.SetActive(false);
-        if (index < dialogue.Length - 1)
-        {
-            index++;
-            dialogueText.text = "";
-            isTyping = StartCoroutine(Typing());
-        }
-        else
-        {
-            zeroText();
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
+        if (other.CompareTag("Player"))
             playerInRange = true;
-        }
     }
-    void OnTriggerExit2D(Collider2D collision)
+
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
-        {
+        if (other.CompareTag("Player"))
             playerInRange = false;
-            zeroText();
-        }
     }
 }
